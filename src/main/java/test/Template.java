f2e07dbc3d29685360ae928f43cda1eb99470023
Network_Attack <javoxir8177@gmail.com>
2022-07-12 21:52:35 -0700

    Procfile

--- src/main/java/test/Template.java
+++ src/main/java/test/Template.java
@@ -2,5 +2,5 @@ package test;
 
 public interface Template {
     String BOT_TOKEN = "5313239264:AAG4wiAnnlHL4TeEss7JNHp66pcrEUdiX1M";
-    String BOT_USER = "t.me/Qoravul_net_bot.";
+    String BOT_USER = "t.me/Qoravul_net_bot";
 }
